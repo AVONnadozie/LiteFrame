880c15449f3c218efc537ff7808db4085a017b14
Victor Anuebunwa <anuebunwa.victor@gmail.com>
2017-12-30 20:01:21 +0100

    Collection

--- core/LiteFrame/Collection.php
+++ core/LiteFrame/Collection.php
@@ -2,14 +2,116 @@
 
 namespace LiteFrame;
 
-/**
- * Description of Collection.
- *
- * @author Victor Anuebunwa
- */
-class Collection
+class Collection implements \Iterator, \ArrayAccess, \Countable
 {
-    public function count($param)
+
+    protected $current = 0;
+    protected $items = [];
+    protected $keys = [];
+
+    function __construct(array $items)
     {
+        $this->current = 0;
+        $this->items = $items;
+        $this->keys = [];
     }
+
+    /**
+     * 
+     * @param type $key
+     * @return \Models\Project
+     */
+    public function get($key)
+    {
+        if (isset($this->items[$key])) {
+            return $this->items[$key];
+        }
+        return null;
+    }
+
+    public function current()
+    {
+        return $this->get($this->current);
+    }
+
+    public function key()
+    {
+        return $this->current;
+    }
+
+    public function next()
+    {
+        ++$this->current;
+    }
+
+    public function rewind()
+    {
+        $this->current = 0;
+    }
+
+    public function valid()
+    {
+        return isset($this->items[$this->current]);
+    }
+
+    
+    public function offsetExists($offset)
+    {
+        if (!isset($this->keys[$offset])) {
+            return false;
+        }
+
+        $key = $this->keys[$this->current];
+        return isset($this->items[$key]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$this->keys[$offset]] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if ($this->offsetExists($offset)) {
+            unset($this->items[$this->keys[$offset]]);
+            unset($this->keys[$offset]);
+        }
+    }
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    /**
+     * Paginate collection items
+     * @param int $perPage
+     * @param int $page
+     * @return \LiteFrame\Collection
+     */
+    public function paginate($perPage, $page = null)
+    {
+        if (!$page) {
+            $page = request('page');
+        }
+
+//        $new_items = 
+
+        return new Collection($new_items);
+    }
+
+    /**
+     * Get first element in collection
+     * @return mixed
+     */
+    public function first()
+    {
+        return isset($this->keys[0]) ? $this->items[$this->keys[0]] : null;
+    }
+
 }

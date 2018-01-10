a4c7a05bc0ddab32335bbfe6a2dbb618503f0452
Victor Anuebunwa <anuebunwa.victor@gmail.com>
2018-01-10 15:11:36 +0100

    Added a base class for PHPUnit TestCase

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
-class ExampleTest extends \PHPUnit\Framework\TestCase
+
+class ExampleTest extends TestCase
 {
     /**
      * A basic functional test example.
---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/**
+ * Base PHPUnit test class
+ *
+ */
+class TestCase extends PHPUnitTestCase
+{
+
+    /**
+     * @provider
+     */
+    public function mockRequest()
+    {
+        
+    }
+
+}
